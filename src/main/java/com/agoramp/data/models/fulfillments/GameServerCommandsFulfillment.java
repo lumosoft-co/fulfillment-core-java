9069c47e8b81cbff8f124fc20c5b37bb9e010c8e
Justin Schreiber <justintschreiber@gmail.com>
2023-04-12 20:53:10 -0700

    add command info

--- src/main/java/com/agoramp/data/models/fulfillments/GameServerCommandsFulfillment.java
+++ src/main/java/com/agoramp/data/models/fulfillments/GameServerCommandsFulfillment.java
@@ -1,5 +1,6 @@
 package com.agoramp.data.models.fulfillments;
 
+import com.agoramp.data.models.common.OnlineStatus;
 import lombok.Data;
 import lombok.Getter;
 
@@ -10,8 +11,10 @@ public class GameServerCommandsFulfillment {
     private List<Command> commands;
     private String target;
 
+    @Getter
     public static class Command {
-        @Getter
         private String command;
+        private OnlineStatus requiredStatus;
+        private Integer requiredSlots;
     }
 }
