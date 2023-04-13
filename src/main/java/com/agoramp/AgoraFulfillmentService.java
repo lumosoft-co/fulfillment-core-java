2afe330ac965df6a412ac0ef142a0e357d8f1905
Justin Schreiber <justintschreiber@gmail.com>
2023-04-12 20:38:02 -0700

    api deps, create config if missing

--- src/main/java/com/agoramp/AgoraFulfillmentService.java
+++ src/main/java/com/agoramp/AgoraFulfillmentService.java
@@ -5,12 +5,11 @@ import com.agoramp.controller.WebhookController;
 import com.agoramp.data.FulfillmentDestinationConfig;
 import com.agoramp.error.ServiceAlreadyInitializedException;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lombok.AccessLevel;
 import lombok.Getter;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 
 public enum AgoraFulfillmentService {
     INSTANCE;
@@ -20,12 +19,27 @@ public enum AgoraFulfillmentService {
     @Getter(AccessLevel.PACKAGE)
     private FulfillmentExecutor executor;
 
-    public void initializeFromFile(File file, FulfillmentExecutor executor) throws FileNotFoundException, ServiceAlreadyInitializedException {
-        initialize(new Gson().fromJson(new FileReader(file), FulfillmentDestinationConfig.class), executor);
+    public void initializeFromFile(File file, FulfillmentExecutor executor) throws ServiceAlreadyInitializedException, IOException {
+        try {
+            initialize(new Gson().fromJson(new FileReader(file), FulfillmentDestinationConfig.class), executor);
+        } catch (FileNotFoundException e) {
+            file.getParentFile().mkdirs();
+            FulfillmentDestinationConfig config = new FulfillmentDestinationConfig(null);
+            String json = new GsonBuilder()
+                    .setPrettyPrinting()
+                    .create()
+                    .toJson(config);
+            FileWriter writer = new FileWriter(file);
+            writer.write(json);
+            writer.flush();
+            writer.close();
+            initialize(config, executor);
+        }
     }
 
     public void initialize(FulfillmentDestinationConfig config, FulfillmentExecutor executor) throws ServiceAlreadyInitializedException {
         if (this.config != null) throw new ServiceAlreadyInitializedException();
+        if (config == null || config.getSecret() == null) throw new Error("Secret not defined");
         this.config = config;
         this.executor = executor;
         if (config.getPort() != null) {
