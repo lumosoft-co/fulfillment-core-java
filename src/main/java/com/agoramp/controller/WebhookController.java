5fa995ee6429d3bc221fc452ff99b70e1fb3484e
Justin Schreiber <justintschreiber@gmail.com>
2023-05-08 13:57:09 -0700

    Auto bind tcp server to Agora shop to automatically receive webhooks

--- src/main/java/com/agoramp/controller/WebhookController.java
+++ src/main/java/com/agoramp/controller/WebhookController.java
@@ -1,28 +1,45 @@
 package com.agoramp.controller;
 
+import com.agoramp.AgoraFulfillmentService;
 import com.agoramp.FulfillmentReceiver;
 import com.agoramp.util.AgoraWebhookStatus;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
 import reactor.netty.DisposableServer;
 import reactor.netty.NettyInbound;
 import reactor.netty.NettyOutbound;
+import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpServer;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 public enum WebhookController implements FulfillmentReceiver {
     INSTANCE;
 
     private DisposableServer server;
 
-    public void initialize(int port) {
+    public void initialize(int port, String secret) {
         if (server != null) server.disposeNow();
         server = TcpServer.create()
                 .host("0.0.0.0")
                 .port(port)
                 .handle(this::handle)
                 .bindNow();
+
+        HttpClient.create()
+                .baseUrl(Optional.ofNullable(System.getenv("AGORA-API-URL")).orElse("https://api.agoramp.com"))
+                .headers(h -> h.add(HttpHeaderNames.CONTENT_TYPE, "application/json"))
+                .post()
+                .uri("/bind")
+                .send(Mono.just(Unpooled.wrappedBuffer(String.format("{\"destination\":\"%s\", \"port\": %d}", secret, server.port()).getBytes(StandardCharsets.UTF_8))))
+                .response()
+                .filter(r -> r.status() == HttpResponseStatus.OK)
+                .subscribe(r -> System.out.println("Agora webhook support has been bound to port " + server.port()));
     }
 
     public void shutdown() {
